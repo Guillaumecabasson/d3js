12b8baaec77b80d039e8e92098178bf5d77ff7ed
camille7415 <59761654+camille7415@users.noreply.github.com>
2022-03-23 18:25:14 +0100

    Changement graphique commu

--- js/communauteVueGeneral.js
+++ js/communauteVueGeneral.js
@@ -2,12 +2,12 @@ d3.csv("data/partages.csv", function(error, data) {
 
     d3.csv("data/super.csv", function(error, data1) {
    
-        var hauteur = 500;
+        var hauteur = 700;
         var largeur = 1100;
         var marge = {haut: 20, droite:30, bas:20, gauche: 30};
 
         var graphe = d3.forceSimulation()
-            .force("charge", d3.forceManyBody().strength(-200))
+            .force("charge", d3.forceManyBody().strength(-20))
             .force("link", d3.forceLink().id(function(d){return data1.id;}).distance(40))
             .force("x", d3.forceX(largeur / 2))
             .force("y", d3.forceY(hauteur / 2))
@@ -25,10 +25,10 @@ d3.csv("data/partages.csv", function(error, data) {
             .enter()
             .append("line")
             .style("stroke", "grey")
-            .style("stroke-opacity", 0.5)
+            .style("stroke-opacity", 1)
             .attr("class", "link").style("stroke-width", d => d.nbpartages);
 
-        noeud = lien.data(data1)
+        noeud = noeud.data(data1)
             .enter()
             .append("circle")
             .style("stroke", "black")
@@ -38,12 +38,14 @@ d3.csv("data/partages.csv", function(error, data) {
 
         function ticked(){
             lien.attr("x1", function(d){return d.t_id.x;})
-            .attr("y1", function(d){return d.t_id.y;})
-            .attr("x2", function(d){return d.rt_id.x;})
-            .attr("y2", function(d){return d.rt_id.y;});
+                .attr("y1", function(d){return d.t_id.y;})
+                .attr("x2", function(d){return d.rt_id.x;})
+                .attr("y2", function(d){return d.rt_id.y;});
 
             noeud.attr("cx", function(d){return d.x;})
-            .attr("cy", function(d){return d.y;});
+                .attr("cy", function(d){return d.y;});
         }
+
+
     });
 });
