d19ef379818f69e97dd0b1d1de2087a3a3d7e22b
camille7415 <59761654+camille7415@users.noreply.github.com>
2022-03-24 01:26:56 +0100

    individ fini

--- js/communauteVueIndividuelle.js
+++ js/communauteVueIndividuelle.js
@@ -1,41 +1,116 @@
-var width = 600, height = 400;
+var width = 1000, height = 500;
+d3.csv("data/super.csv").then((data12) => {
+    d3.csv("data/partages.csv").then((data) => {
 
-var colorScale = 'orange';
-var xCenter = 300;
+        var elements = data12;
+	    var selectedItem = elements[73];
 
-var numNodes = 100;
-var nodes = d3.range(numNodes).map(function(d, i) {
-	return {
-		radius: Math.random() * 25,
-		category: i % 3
-	}
+        var colorScale = ['orange', 'lightblue', '#B19CD9'];
+        var xCenter = [400, 25, 500];
+
+        var userId = selectedItem.id;
+
+        var dataBis = [];
+
+        data.forEach(element => {
+            if(element.t_id == userId){
+                dataBis.push({id : element.rt_id, nb : element.nbpartages});
+            }
+        });
+
+        var max = d3.max(dataBis, d => Number(d.nb));
+
+        console.log(max);
+
+        console.log(dataBis);
+
+        var data1 = d3.range(dataBis.length).map(function(d, i) {
+            console.log(dataBis[i]);
+            return {
+                id : dataBis[i].id, 
+                nb : dataBis[i].nb,
+                radius : (Number(dataBis[i].nb) / max) * 100
+            }
+        });
+
+        console.log(data1);
+
+        var simulation = d3.forceSimulation(data1)
+            .force('charge', d3.forceManyBody().strength(5))
+            .force('center', d3.forceCenter(xCenter[0], xCenter[1]))
+            .force('collision', d3.forceCollide().radius(function(d) {
+                return d.radius;
+            }))
+            .on('tick', ticked);
+
+        function ticked() {
+            var u = d3.select('svg g')
+                .selectAll('circle')
+                .data(data1)
+                .join('circle')
+                .attr('r', function(d) {
+                    return d.radius;
+                })
+                .style('fill', function(d) {
+                    return colorScale[1];
+                })
+                .attr('cx', function(d) {
+                    return d.x;
+                })
+                .attr('cy', function(d) {
+                    return d.y;
+                });
+        }
+
+        var selector = d3.select("#select2")
+            .append("select")
+            .attr("id","dropdown2")
+            .on("change", function(d){
+                var selectedElement = document.getElementById("dropdown2").value;
+                console.log(selectedElement);
+                userId = selectedElement;
+                dataBis.length = 0;
+
+                data.forEach(element => {
+                    if(element.t_id == userId){
+                        dataBis.push({id : element.rt_id, nb : element.nbpartages});
+                        console.log(element.rt_id);
+                    }
+                });
+
+                max = d3.max(dataBis, d => Number(d.nb));
+                console.log(max);
+console.log(dataBis);
+                data1 = d3.range(dataBis.length).map(function(d, i) {
+                    console.log(dataBis[i]);
+                    return {
+                        id : dataBis[i].id, 
+                        nb : dataBis[i].nb,
+                        radius : (Number(dataBis[i].nb) / max) * 100
+                    }
+                });
+                
+
+                simulation = d3.forceSimulation(data1)
+                    .force('charge', d3.forceManyBody().strength(5))
+                    .force('center', d3.forceCenter(xCenter[0], xCenter[1]))
+                    .force('collision', d3.forceCollide().radius(function(d) {
+                        return d.radius;
+                    }))
+                    .on('tick', ticked);
+        
+            });
+        
+            console.log(elements);
+        // création des option du select avec les en-têtes des entreprises
+        selector.selectAll("option")
+        .data(elements)
+        .enter().append("option")
+        .attr("value", function(d){
+            return d.id;
+        })
+        .text(function(d){
+            return d.id;
+        });
+    });
 });
-
-var simulation = d3.forceSimulation(nodes)
-	.force('charge', d3.forceManyBody().strength(5))
-	.force('x', d3.forceX().x(function(d) {
-		return xCenter[d.category];
-	}))
-	.force('collision', d3.forceCollide().radius(function(d) {
-		return d.radius;
-	}))
-	.on('tick', ticked);
-
-function ticked() {
-	var u = d3.select('svg g')
-		.selectAll('circle')
-		.data(nodes)
-		.join('circle')
-		.attr('r', function(d) {
-			return d.radius;
-		})
-		.style('fill', function(d) {
-			return colorScale[d.category];
-		})
-		.attr('cx', function(d) {
-			return d.x;
-		})
-		.attr('cy', function(d) {
-			return d.y;
-		});
-}
